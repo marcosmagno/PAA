ca53a344b0173fb27c6ae0a83df58cdb07d4a802
Marcos Carvalho <marcosf40@hotmail.com>
2019-04-15 20:50:09 -0300

    Input and Output

---
+++ TP1/run.py
@@ -0,0 +1,18 @@
+"""
+Input
+    Multiplas navaes de maneira conjunta
+    N = Numero de postos de combate encontrados pelo radar
+    M = Quantidade total de teleporte possíveis entre postos de combate
+
+Output
+    R,F,B e T  
+        R = Nave de reconhecimento 
+        F = Nave Frigatas  
+        B = Nave bombardeiros  
+        T = Naves Transporte
+    V = Tempo de vantagem que sua frota possui
+
+
+Exemplo:
+    15 14
+"""
