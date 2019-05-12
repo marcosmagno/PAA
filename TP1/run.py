e7ab937a1ac2bdf6f4d89001df2e1af78ddd7d93
Marcos Carvalho <marcosf40@hotmail.com>
2019-05-12 11:40:44 -0300

    saida formated

--- TP1/run.py
+++ TP1/run.py
@@ -2,7 +2,7 @@ import time
 import sys
 """
 Input
-    Multiplas navaes de maneira conjunta
+    Multiplas naves de maneira conjunta
     N = Numero de postos de combate encontrados pelo radar
     M = Quantidade total de teleporte possíveis entre postos de combate
 
@@ -14,19 +14,11 @@ Input
         c =
         d = 
 
-Output
-    R,F,B e T  
-        R = Nave de reconhecimento 
-        F = Nave Frigatas  
-        B = Nave bombardeiros  
-        T = Naves Transporte
-    V = Tempo de vantagem que sua frota possui
-
 
 Exemplo:
     15 14
 
-Ref: https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/components.html
+
 """
 
 class Node(object):
@@ -34,6 +26,9 @@ class Node(object):
         self.adjacency_list = dict()
     
     def set_adjacency_list(self, u, v):
+        """ 
+            Cria lista de Adjacencia
+        """
         if u not in self.adjacency_list:
             self.adjacency_list[u] = []
         if v not in self.adjacency_list:
@@ -47,93 +42,83 @@ class Node(object):
 
 
 class Related_Components(object):
-    
+    """
+        Implementa o calculo de componentes conexas
+        O algoritmo aqui utilizado, temos como referencia o site abaixo:
+            Ref: https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/components.html
+    """
     def __init__(self, qt_vertex):
         self.qt_vertex = qt_vertex
 
     def UGRAPHcc(self, graph, cc):
-        self.id = 0
+        self.qt_componente_conexa = 0
         self.indexAdja = 0
         self.Transportadores = 0
         self.Reconhecimento = 0
+        self.Frigatas = 0
+        self.Bombardeiros = 0
+        self.countVertice = 0
+        self.countAresta = 0
         for v in range(1, self.qt_vertex + 1): # M + 1
             cc.append(-1)
 
         for v in range(1, self.qt_vertex + 1): # M + 1
             try:
                 if(cc[v-1] == -1):
-                    self.id = self.id +1                    
+                    self.qt_componente_conexa = self.qt_componente_conexa +1                    
                     """ call dfsRcc """
                     self.singleGraph = {}
-                    color = []
-                    returnDfs = self.dfsRcc(graph, cc, v, self.id)
-                    qt_transportadores = self.check_Transportadores(returnDfs)
-                    print("qt_ transportadores", qt_transportadores)
-                    newg = returnDfs.copy()
-                    self.UGRAPHtwoColor(newg, color)
+                    self.countAresta = 0
+                    self.countVertice = 0
+                    returnDfs = self.dfsRcc(graph, cc, v, self.qt_componente_conexa)
+                    qt_ships = self.check_ships(returnDfs, self.countVertice)
             except IndexError as identifier:
                 pass
-        return self.id
+        return str(self.qt_componente_conexa) + "-" + str(qt_ships)
 
     def dfsRcc(self,graph, cc, v, id):
         cc[v-1] = id
         self.singleGraph[v] = []
         self.singleGraph[v] = graph[v]
+        self.countVertice = self.countVertice + 1
         try:
             for a in graph[v]:
                 if(cc[a-1] == -1):
                     self.dfsRcc(graph, cc, a, id)
         except KeyError as identifier:
             pass
-        
         return self.singleGraph
         
-    def check_Transportadores(self, graph):
+    def check_ships(self, graph, numberVertice):
         grau = []
+        soma = 0
         for k, v in graph.items():
             grau.append(len(v))
+
+        # Reconhece nave do tipo Transportadores
+        # Se todos os vertices tiverem grau 2, entao, e um Transportador    
         if (all(s == 2 for s in grau)):
-            self.Transportadores = self.Transportadores + 1  
-        
-        
+            self.Transportadores = self.Transportadores + 1
+
+        for i in grau:
+            soma = soma + i
+
+        # Reconhece nave do tipo Reconhecimento
+        # Se todos os vertires a partir da segunda posicao ate a penultima,
+        # tiverem grau 2, entao, eh um caminho simples, portanto, uma nave do tipo Reconhecimento
         if grau[0] == 1 and grau[-1] == 1:
             del grau[0]
             del grau[-1]
-            #print("Depois",grau)
             if (all(s == 2 for s in grau)):
                 self.Reconhecimento = self.Reconhecimento + 1
         
-        return str(self.Transportadores) + " - " + str(self.Reconhecimento)
+        if grau[0] != grau[-1] or 3 in grau or 5 in grau or 6 in grau:
+            if (numberVertice >= int(soma/2)):
+                self.Frigatas = self.Frigatas + 1
 
-    def UGRAPHtwoColor(self, graph, color):
-        for i in range(1, len(graph) + 1):
-            color.append(-1)
+        
+        return str(self.Reconhecimento) + "-" + str(self.Frigatas) + "-" + str(self.Bombardeiros) + "-" + str(self.Transportadores)
 
-        for v in range(1, len(graph) + 1):
-            if color[v - 1] == -1:
-                if (self.dfsRcolor(graph, v, color, 0) == False):
-                    return False
-                else:
-                    return True
-
-    def dfsRcolor(self, graph, v, color, c):
-        color[v - 1] = c 
-        i = v - 1
-        print("VERTICE", v)
-        print("COLOR", color)
-        print("GRAPH", graph)
-        for a in graph.values():
-            
-            w = a[i]
-            i + 1 
-            print("VARIAVEL A", a)
-            print("VARIAVEL W", w)
-            if(color[w - 1] == -1):
-                print("CHAMOU REC DFS")
-                self.dfsRcolor(graph, w, color, 1-c)
-            elif(color[w] == c):
-                return False
-        return True
         
 
 class Input(object):
@@ -149,6 +134,14 @@ class Input(object):
             N linhas : O tripulante c deve retornar para o posto correto d
                 1 <= c <= N
                 1 <= d <= N
+
+            Output
+                R,F,B e T  
+                    R = Nave de reconhecimento 
+                    F = Nave Frigatas  
+                    B = Nave bombardeiros  
+                    T = Naves Transporte
+                V = Tempo de vantagem que sua frota possui                
         """
         get_file = open(file_name,"r")
         return get_file
@@ -164,12 +157,26 @@ def main():
     node = Node()
     components = Related_Components(int(qt_local_tripulante))
     vector = []
-    
+
+    # Naves
+    Transportadores = 0
+    Reconhecimento = 0
+    Frigatas = 0
+    Bombardeiros = 0
+    sum_qtComponents = 0
+
     for i in range(0, int(qt_teleportes)):
         var = obj_file.readline()
         node.set_adjacency_list(int(var.split(" ")[0]), int(var.split(" ")[1]))
     qt_Components = components.UGRAPHcc(node.get_adjacency_list(), vector)
-    print(qt_Components)
 
+    Reconhecimento = qt_Components.split("-")[1]
+    Frigatas = qt_Components.split("-")[2]
+    Bombardeiros = qt_Components.split("-")[3]
+    Transportadores = qt_Components.split("-")[4]
+    sum_qtComponents = int(Reconhecimento) + int(Frigatas) + int(Bombardeiros) + int(Transportadores)
+    Bombardeiros = int(qt_Components.split("-")[0]) - int(sum_qtComponents)
+    
+    print(str(Reconhecimento) + " " + str(Frigatas) + " " + str(Bombardeiros) + " " + str(Transportadores))
 if __name__ == "__main__":
     main()
