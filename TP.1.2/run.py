78d95fcbd7f79529387c2636207723abeeac73a6
Marcos Carvalho <marcosf40@hotmail.com>
2019-10-05 15:56:10 -0300

    create graph

--- TP.1.2/run.py
+++ TP.1.2/run.py
@@ -16,34 +16,26 @@ Output
         para transformar a configuração inicial na configuração final
 """
 
-class Node(object):
+class Graph(object):
     def __init__(self):
-        self.adjacency_list = dict()
-    
-    def set_adjacency_list(self, u, v):
+        
+        self.graph = dict()
+    def set_adjacency_list(self,u, v):
         """ 
             Cria lista de Adjacencia
         """
-        if u not in self.adjacency_list:
-            self.adjacency_list[u] = []
-        if v not in self.adjacency_list:
-            self.adjacency_list[v] = []    
-        self.adjacency_list[u].append(v)        
-        self.adjacency_list[v].append(u)
+        #print(u, v)
+
+        if v not in self.graph:
+            self.graph[v] = [[u,2]]
+        else:
+            self.graph[v].append([u,2])    
+        
+    def get_graph(self):
+        return self.graph
 
 
-    def get_adjacency_list(self):
-        return self.adjacency_list
 
-
-class Related_Components(object):
-    """
-        Implementa o calculo de componentes conexas
-        O algoritmo aqui utilizado, temos como referencia o site abaixo:
-            Ref: https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/components.html
-    """
-    def __init__(self, qt_vertex):
-        self.qt_vertex = qt_vertex
 
 class Input(object):
     def read_file(self, file_name):
@@ -52,75 +44,114 @@ class Input(object):
 
 def main():
     file_ = Input()
+    
     file_recv = sys.argv[1]
     obj_file = file_.read_file(str(file_recv))
     first_line = obj_file.readlines()
     linhas = first_line[0].split(" ")[0]
     colunas = first_line[0].split(" ")[1]
-    
     pesos = first_line[1]
-   
     i = 0
     dic_permutacao = {}
-    graph = {}
-    for p in permutacao([1,2,3,4,5,6,7,8,9]):
-    #for p in permutacao([1,2,3,4]):
+    
+    #for p in permutacao([1,2,3,4,5,6,7,8,9]):
+    #for p in permutacao([1,2,3,4,5,6,7,8]):
+    for p in permutacao([1,2,3,4]):
         i = i + 1
-        dic_permutacao[i] = list(p)
-    #create_Graph(dic_permutacao, 2, 2)
-    create_Graph(dic_permutacao, 3, 3)
+        dic_permutacao[str(p)] = i
+    create_Graph(dic_permutacao, 2, 2)
+   
+    #create_Graph(dic_permutacao, 3, 3)
+    #create_Graph(dic_permutacao, 4, 2)
 
 #n linhas m colunas
-def create_Graph(p, l, c):
-    tmp = []
+def create_Graph(permutations, l, c):
     matriz = []
     matrizResultante = []
-    controleLinha = 0
-    for k, v in p.items():
+    graph = Graph()
+    #print(permutations)
+    for k, v in permutations.items():
         n = l
-        matriz = []
-        len_l = len(v)
-        for i in range(n):
-            start = int(i*len_l/n)
-            end = int((i+1)*len_l/n)
-            matriz.append(v[start:end])
-        print("\n\n\n\n")
-        print("Vetor Inicial", k, v)
+        r = k.replace("[","")
+        r = r.replace("]","")
+        resultList = list(map(int, r.split(",")))
+        #resultList = [1,2,3,4,5,6,7,8,9]
+        matriz = [resultList[i:i+c] for i in range(0, len(resultList), c)]
+        #print("\n\n\n")
+        #print(k, v)
+        #matriz = [[1,2,3],[3,4,6],[7,8,9]]
+        #len_l = len(v)
+        #for i in range(n):
+        #    start = int(i*len_l/n)
+        #    end = int((i+1)*len_l/n)
+        #    matriz.append(v[start:end])
         # permutacoes
-        
+
         for i in range(0,l):
             for j in range(0,c-1):
-                
-                matrizResultante = [lst[:] for lst in matriz] #copy.deepcopy(matriz)
-                #print(matriz[i][j])
+                #matrizResultante = [lst[:] for lst in matriz] #copy.deepcopy(matriz)
                 #print(matriz[i][j+1])
+                #aux = matriz[i][j] # 1
+                #matrizResultante[i][j] = matriz[i][j+1] #2
+                #matrizResultante[i][j+1] = aux
+                #print("Matriz Inicial Horizontal: ", matriz)
+                # tentando fazer na mesma matriz
+                #print("Custo Troca", matriz[i][j]+ matriz[i][j+1])
+                
                 aux = matriz[i][j] # 1
-                matrizResultante[i][j] = matriz[i][j+1] #2
-                matrizResultante[i][j+1] = aux
-                print("Matriz Inicial Horizontal: ", matriz)
-                espande_Permutacao(matrizResultante)
+                matriz[i][j] = matriz[i][j+1]
+                matriz[i][j+1] = aux
+                u = espande_Permutacao(matriz, permutations)
+                graph.set_adjacency_list(u,v)
 
-                #print("matriz Trocada", matrizResultante)
+                #print("matriz Trocada ", matriz)
 
-        #print("\n")
+                aux = matriz[i][j]
+                matriz[i][j] = matriz[i][j+1]
+                matriz[i][j+1] = aux
+
+
         for i in range(0,l-1):
             for j in range(0,c):
-                matrizResultante = [lst[:] for lst in matriz] #copy.deepcopy(matriz)
+                #matrizResultante = [lst[:] for lst in matriz] #copy.deepcopy(matriz)
+                #print("Matriz Inicial Vertical: ", matriz)
+                #print("Custo Troca", matriz[i][j]+ matriz[i+1][j])
                 aux = matriz[i][j] # 1
-                matrizResultante[i][j] = matriz[i+1][j] #3
-                matrizResultante[i+1][j] = aux
-                print("Matriz Inicial Vertical: ", matriz)
-                espande_Permutacao(matrizResultante)
-                                
-        
-        
+                matriz[i][j] = matriz[i+1][j] #3
+                matriz[i+1][j] = aux
+                u = espande_Permutacao(matriz,permutations)
+                graph.set_adjacency_list(u,v)
+                aux = matriz[i][j]
+                matriz[i][j] = matriz[i+1][j]
+                matriz[i+1][j] = aux     
 
+    for k, v in graph.get_graph().items():
+        print(k, v)
 
-def espande_Permutacao(matrizResultante):
+def relax(u, v, w):
+    """
+        if v.d > u.d + w(u,v)
+            v.d = u.d + w(u,v)
+    """
     
-    print("matriz Trocada", matrizResultante)
-    time.sleep(2)
+def initSingleSource(G,s):
+    """ 
+        for cada vertice v in V[G]
+            v.d = infinito
+        s = 0
+    """
+def espande_Permutacao(matrizResultante, allpermutations):
+    """ 
+        return key from dict, if matrizResultante is in allpermutations
+    """
+    #print("matriz Trocada", matrizResultante)
+    array_toCheck = []
+    for i in matrizResultante:
+        for j in i:
+            array_toCheck.append(j)
+    return allpermutations.get(str(array_toCheck))
 
+    
     
 
 def permutacao(matrizAtual, c=0):
